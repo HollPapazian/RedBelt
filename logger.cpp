bc59b3e6c29cb1aee61adc7d958b2278d7581e50
Volkov Anton <anton@wolkow.ru>
2018-09-13 11:53:03 +0300

    Спортсмены

--- logger.cpp
+++ logger.cpp
@@ -1,94 +1,41 @@
 #include "test_runner.h"
-
+#include "profile.h"
+#include <list>
+#include <vector>
+#include <iostream>
 #include <algorithm>
-#include <numeric>
+#include <random>
 using namespace std;
 
-template <typename T>
-void Swap(T* first, T* second) {
-	T temp_ptr = *first;
-	*first = *second;
-	*second = temp_ptr;
-};
 
-template <typename T>
-void SortPointers(vector<T*>& pointers) {
-	sort(pointers.begin(), pointers.end(),[](T* lhr, T* rhr) {return *lhr < *rhr;});
-};
-
-template <typename T>
-void ReversedCopy(T* source, size_t count, T* destination)
-{
-	T* temp_ptr = new T[count];
-	for (size_t i = 0; i < count; ++i){
-		//cerr << "i = " << i << " count = " << count << endl;
-		temp_ptr[i] = source[count - 1 - i];
-	}
-	for (size_t i = 0; i < count; ++i){
-		//cerr << "i = " << i << " count = " << count << endl;
-		destination[i] = temp_ptr[i];
-	}
-
-};
-
-void TestSwap() {
-  int a = 1;
-  int b = 2;
-  Swap(&a, &b);
-  ASSERT_EQUAL(a, 2);
-  ASSERT_EQUAL(b, 1);
-
-  string h = "world";
-  string w = "hello";
-  Swap(&h, &w);
-  ASSERT_EQUAL(h, "hello");
-  ASSERT_EQUAL(w, "world");
-}
-
-void TestSortPointers() {
-  int one = 1;
-  int two = 2;
-  int three = 3;
-
-  vector<int*> pointers;
-  pointers.push_back(&two);
-  pointers.push_back(&three);
-  pointers.push_back(&one);
-
-  SortPointers(pointers);
-
-  ASSERT_EQUAL(pointers.size(), 3u);
-  ASSERT_EQUAL(*pointers[0], 1);
-  ASSERT_EQUAL(*pointers[1], 2);
-  ASSERT_EQUAL(*pointers[2], 3);
-}
-
-void TestReverseCopy() {
-  const size_t count = 7;
-
-  int* source = new int[count];
-  int* dest = new int[count];
-
-  for (size_t i = 0; i < count; ++i) {
-    source[i] = i + 1;
-  }
-  ReversedCopy(source, count, dest);
-  const vector<int> expected1 = {7, 6, 5, 4, 3, 2, 1};
-  ASSERT_EQUAL(vector<int>(dest, dest + count), expected1);
-
-  // Области памяти могут перекрываться
-  ReversedCopy(source, count - 1, source + 1);
-  const vector<int> expected2 = {1, 6, 5, 4, 3, 2, 1};
-  ASSERT_EQUAL(vector<int>(source, source + count), expected2);
-
-  delete[] dest;
-  delete[] source;
-}
 
 int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestSwap);
-  RUN_TEST(tr, TestSortPointers);
-  RUN_TEST(tr, TestReverseCopy);
-  return 0;
+
+using list_it = list<int>::iterator;
+list<int> list_s;
+
+
+
+int q;
+cin >> q;
+vector<list_it> sportsmen_vec(100000, list_s.end());
+sportsmen_vec.reserve(100000);
+//if (sportsmen_vec[10] == list_s.end())
+//cerr << "fasfa";
+
+for (auto i = 0; i < q;++i) {
+	int num;
+	int pos;
+	cin >> num;
+	cin >> pos;
+	auto it = list_s.insert(sportsmen_vec[pos], num);
+	//cout << *it << endl;
+	sportsmen_vec[num] = it;
+
+}
+
+for (auto a : list_s)
+cout << a << " ";
+
+return 0;
 }
