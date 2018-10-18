ae5833b3ac15796a8c46a106f29a014f008cb357
Volkov Anton <anton@wolkow.ru>
2018-10-18 10:03:32 +0300

    Priory container 1/1

--- logger.cpp
+++ logger.cpp
@@ -4,92 +4,84 @@
 #include <iterator>
 #include <memory>
 #include <set>
+#include <map>
 #include <utility>
 #include <vector>
-#include <list>
-#include <string>
 
 using namespace std;
 
 template <typename T>
 class PriorityCollection {
 public:
-	PriorityCollection()=default;
-  using Id = pair<T, int>*;/* тип, используемый для идентификаторов */;
+//	PriorityCollection(){
+//		data_values.reserve(1000000);
+//		data_link.reserve(1000000);
+//	}
+  using Id = int /* тип, используемый для идентификаторов */;
 
   // Добавить объект с нулевым приоритетом
   // с помощью перемещения и вернуть его идентификатор
   Id Add(T object){
-  data.push_back(make_pair(move(object), 0));
-  data_it.insert(&data.back());
-  ++priority_count[0];
-  return &data.back();
+  data_values.push_back(move(object));
+  data_link.push_back(data.insert(make_pair(0,data_values.size()-1)).first);
+  return data_values.size()-1;
   }
+
   // Добавить все элементы диапазона [range_begin, range_end)
   // с помощью перемещения, записав выданные им идентификаторы
   // в диапазон [ids_begin, ...)
   template <typename ObjInputIt, typename IdOutputIt>
   void Add(ObjInputIt range_begin, ObjInputIt range_end,
            IdOutputIt ids_begin){
-  for (auto i = range_begin; i != range_end; ++i) {
-	  data.push_back(make_pair(move(*i), 0));
-	  data_it.insert(&data.back());
-	  *ids_begin = &data.back();
-	  ++ids_begin;
-	  ++priority_count[0];
+	  for (auto a = range_begin; a != range_end; ++a) {
+		  data_values.push_back(move(*a));
+		  data_link.push_back(data.insert(make_pair(0,data_values.size()-1)).first);
+		  *(ids_begin++) = data_values.size()-1;
+	  }
   }
 
-  }
   // Определить, принадлежит ли идентификатор какому-либо
   // хранящемуся в контейнере объекту
   bool IsValid(Id id) const {
-	  return data_it.count(id);
+	  return !(data_link[id] == data.end() || id >= data_link.size());
   }
 
   // Получить объект по идентификатору
   const T& Get(Id id) const {
-	  return id->first;
+	  return data_values[id];
   }
 
   // Увеличить приоритет объекта на 1
   void Promote(Id id) {
-	  int temp = ++(id->second);
-	  ++priority_count[temp];
-	  --priority_count[--temp];
-	 // cerr << "Promote" << id->first << " " << id->second << endl;
-//	  if (temp > max_priority)
-//		  max_priority = temp;
+	  int old_pr = (*data_link[id]).first;
+	  data.erase(data_link[id]);
+	  data_link[id] = (data.insert(make_pair(++old_pr,id)).first);
   }
 
   // Получить объект с максимальным приоритетом и его приоритет
-  pair<const T&, int> GetMax() const {
-	int max_priority = prev(priority_count.end(),1)->first;
-	return *find_if(data.rbegin(), data.rend(), [&](const auto& i){return i.second == max_priority;});
+  pair<const T&, int> GetMax() const{
+	  int max_pr = (*data.rbegin()).first;
+	  int max_id = (*data.rbegin()).second;
+	 // cerr << data_values[max_id] << endl;
+	  const T& temp = data_values[max_id];
+	  return {temp, max_pr};
   }
 
   // Аналогично GetMax, но удаляет элемент из контейнера
-  pair<T, int> PopMax() {
-	  int max_priority = prev(priority_count.end(),1)->first;
-	  auto it = --data.end();
-	  for (; it != data.begin(); it--)
-		  if ((*it).second == max_priority) break;
-
-	  data_it.erase(&*it);
-	  auto temp = move(*it);
-	  data.erase(it);
-
-	  --priority_count[max_priority];
-	  if (priority_count[max_priority] == 0)
-		  priority_count.erase(max_priority);
-	  return move(temp);
+  pair<T, int> PopMax(){
+	  int max_pr = (*data.rbegin()).first;
+	  int max_id = (*data.rbegin()).second;
+	  data.erase(prev(data.end(), 1));
+	  //auto temp = move(data_values[max_id]);
+	  data_link[max_id] = data.end();
+	  return {move(data_values[max_id]), max_pr};
   }
 
 private:
-  list<pair<T, int>> data;
-  set<pair<T, int>*> data_it;
-  //int max_priority = 0;
-  map<int,int> priority_count;
   // Приватные поля и методы
+  set<pair<int, int>> data; // приоритет и id
+  vector<set<pair<int, int>>::iterator> data_link; // хранилище итераторов пар приоритета и ID
+  vector<T> data_values; // Хранилище данных
 };
 
 
@@ -103,7 +95,6 @@ public:
 };
 
 void TestNoCopy() {
-
   PriorityCollection<StringNonCopyable> strings;
   const auto white_id = strings.Add("white");
   const auto yellow_id = strings.Add("yellow");
@@ -116,47 +107,57 @@ void TestNoCopy() {
   strings.Promote(yellow_id);
 
   {
+    const auto item = strings.GetMax();
+    ASSERT_EQUAL(item.first, "red");
+    ASSERT_EQUAL(item.second, 2);
+  }
+  {
     const auto item = strings.PopMax();
     ASSERT_EQUAL(item.first, "red");
     ASSERT_EQUAL(item.second, 2);
   }
-
   {
     const auto item = strings.PopMax();
     ASSERT_EQUAL(item.first, "yellow");
     ASSERT_EQUAL(item.second, 2);
   }
-
   {
     const auto item = strings.PopMax();
     ASSERT_EQUAL(item.first, "white");
     ASSERT_EQUAL(item.second, 0);
   }
-
 }
 
+void TestMy() {
+  PriorityCollection<int> strings;
+  const auto white_id = strings.Add(1);
+  const auto yellow_id = strings.Add(2);
+  const auto red_id = strings.Add(3);
 
-void MyTest() {
-
-  PriorityCollection<StringNonCopyable> strings;
-  const auto white_id = strings.Add("white");
-  const auto yellow_id = strings.Add("yellow");
-  const auto red_id = strings.Add("red");
-
-  strings.Promote(white_id);
-
-  {
-    const auto item = strings.PopMax();
-    ASSERT_EQUAL(item.first, "white");
-    ASSERT_EQUAL(item.second, 1);
+  strings.Promote(yellow_id);
+  for (int i = 0; i < 2; ++i) {
+    strings.Promote(red_id);
   }
-
-  strings.Promote(red_id);
-  strings.Promote(red_id);
+  strings.Promote(yellow_id);
+  {
+    const auto item = strings.GetMax();
+    ASSERT_EQUAL(item.first, 3);
+    ASSERT_EQUAL(item.second, 2);
+  }
   {
     const auto item = strings.PopMax();
-    ASSERT_EQUAL(item.first, "red");
+    ASSERT_EQUAL(item.first, 3);
     ASSERT_EQUAL(item.second, 2);
+  }
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, 2);
+    ASSERT_EQUAL(item.second, 2);
+  }
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, 1);
+    ASSERT_EQUAL(item.second, 0);
   }
 
 }
@@ -164,6 +165,6 @@ void MyTest() {
 int main() {
   TestRunner tr;
   RUN_TEST(tr, TestNoCopy);
-  RUN_TEST(tr, MyTest);
+  RUN_TEST(tr, TestMy);
   return 0;
 }
